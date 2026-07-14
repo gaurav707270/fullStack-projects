ea474863348d99eec40c963392f20cc3a16e8e61
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-14 13:52:24 +0530

    Update app.js

--- YT-GENAI/src/app.js
+++ YT-GENAI/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    // res.status(200).json({
+    res.status(200).json({
         // success: true,
         // message: "Server is running successfully"
     // });
