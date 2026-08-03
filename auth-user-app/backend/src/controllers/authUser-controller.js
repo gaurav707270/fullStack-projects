8c1a63e6a47e21022d17ff25aa222358eba90e55
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-03 18:34:35 +0530

    03/08/2026

--- auth-user-app/backend/src/controllers/authUser-controller.js
+++ auth-user-app/backend/src/controllers/authUser-controller.js
@@ -1,5 +1,6 @@
 import authUser from "../models/authUser-module.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const fetchAuthUser = async (req, res) => {
     try {
@@ -50,20 +51,44 @@ export const signUPAuthUser = async (req, res) => {
 export const signInAuthUser = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const singInUser = await authUser.findOne({ email, password });
+        const singInUser = await authUser.findOne({ email });
 
         console.log("user singIn successfully")
 
         if (!singInUser) {
             res.status(400).json({
                 status: false,
-                message: "email or password wrong"
+                message: "user not found"
             });
-        } else {
-            res.status(200).json({
-                status: true,
-                message: "user singIn successfully "
-            });
+
+            const isMatch = await bcrypt.compare(password, authUser.password)
+
+            if (isMatch) {
+
+                const token = jwt.sign({ userId: authUser._id, email: authUser.email },
+                    "!@#$%^&*()",
+                    { expiresIn: "1h" }
+                );
+
+                res.cookie("token", token, {
+                    httpOnly: true,
+                    maxAge: 1000 * 60 * 60
+                })
+
+                // const token = jwt.sign(authUser,"S@chin7072",{expiresIn:"1h"})
+
+
+                res.status(200).json({
+                    status: true,
+                    message: "user singIn successfully ",
+                    token
+                })
+            } else {
+                res.status(400).json({
+                    status: false,
+                    message: "email or password wrong"
+                });
+            };
         }
 
     } catch (err) {
