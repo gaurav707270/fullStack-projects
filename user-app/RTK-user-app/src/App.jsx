5ba942a3a5e04f11460a3a86523cc75346dea695
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-22 12:47:53 +0530

    Update App.jsx

--- user-app/RTK-user-app/src/App.jsx
+++ user-app/RTK-user-app/src/App.jsx
@@ -10,7 +10,7 @@ import Users from './screen/Users';
   // return (
     <div>
       {/* <Routes> */}
-        {/* <Route path='/' element={<SignIn />} /> */}
+      <Route path='/' element={<SignIn />} /> 
         {/* <Route path='/signup' element={<SignUp />} /> */}
         <Route path='/users' element={<Users />} />
 
