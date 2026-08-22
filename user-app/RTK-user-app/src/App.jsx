ed9a687a4477eb23fb88bdf1df54b1caabc17c84
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-22 12:45:32 +0530

    Update App.jsx

--- user-app/RTK-user-app/src/App.jsx
+++ user-app/RTK-user-app/src/App.jsx
@@ -7,7 +7,7 @@
 // import Users from './screen/Users';
 
 // export default function App() {
-  return (
+  // return (
     <div>
       {/* <Routes> */}
         {/* <Route path='/' element={<SignIn />} /> */}
