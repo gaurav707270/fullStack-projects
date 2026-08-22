60b202120b55558c626f86d8695dc129e9679238
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-22 12:45:12 +0530

    Update App.jsx

--- user-app/RTK-user-app/src/App.jsx
+++ user-app/RTK-user-app/src/App.jsx
@@ -11,7 +11,7 @@
     <div>
       {/* <Routes> */}
         {/* <Route path='/' element={<SignIn />} /> */}
-        <Route path='/signup' element={<SignUp />} />
+        {/* <Route path='/signup' element={<SignUp />} /> */}
         <Route path='/users' element={<Users />} />
 
       </Routes>
